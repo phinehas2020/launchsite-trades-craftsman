f1ac491fdb84a41f02ceb4799b9d699f18595a8f
Phinehas Adams <phinehasmadams@icloud.com>
2026-07-16 11:23:09 -0500

    Restore full website as main experience

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,6 @@ export default defineConfig({
         portfolio: resolve(__dirname, 'portfolio/index.html'),
         team: resolve(__dirname, 'team/index.html'),
         contact: resolve(__dirname, 'contact/index.html'),
-        fundraiser: resolve(__dirname, 'fundraiser/index.html'),
       },
     },
   },
